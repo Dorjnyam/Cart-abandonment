5cd9c12eaec2cc50da106b81ce96005ee093c3a8
Dorjnyam Erdenetsogt <mjldoko11@gmail.com>
2026-05-14 00:29:07 +0800

    Silence set-state-in-effect lint for installation page load()

--- cart_analytic/src/app/installation/page.tsx
+++ cart_analytic/src/app/installation/page.tsx
@@ -70,6 +70,7 @@ export default function InstallationPage() {
   }, [lang]);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     void load();
   }, [load]);
 
